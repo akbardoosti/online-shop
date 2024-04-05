1741581b748a9f860cad090dab5d4c62406d9071
Akbar Doosti <83670691+akbardoosti@users.noreply.github.com>
2024-04-05 20:46:42 +0330

    designed 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,14 @@
-import Link from 'next/link'
-
+import './styles/404.css'
 export default function NotFound() {
-  return <div>
-      <h1>Not found – 404!</h1>
-      <div>
-        <Link href="/">Go back to Home</Link>
-      </div>
-  </div>
+    return <div id="notfound">
+        <div className="notfound">
+            <div className=" notfound-404">
+                <h1 className='text-orange-600'>404</h1>
+                <h2 className='site-font text-gray-200'>چنین صفحه ای وجود ندارد</h2>
+            </div>
+            <a href="/" className='site-font rounded bg-fuchsia-700 text-white'>
+                بازگشت به صفحه اصلی
+            </a>
+        </div>
+    </div>
 }
