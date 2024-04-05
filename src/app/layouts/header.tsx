9fb346fb7b302d6aa2ed5c10e8ef9150334f8b86
Akbar Doosti <83670691+akbardoosti@users.noreply.github.com>
2024-04-05 23:10:38 +0330

    designed cart page

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -66,11 +66,11 @@ const Header: React.FC<{}> = (() => {
                     </button>
                 </div>
                 <div>
-                    <button>
+                    <Link href='/cart'>
                         <svg className="w-8 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.4" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
                         </svg>
-                    </button>
+                    </Link>
                 </div>
             </div>
             <MenuSlide isOpen={isSlideOverOpen} onClose={toggleSlideOver} />
